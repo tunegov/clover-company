1a99de3d097bdc7ad29cdd799f3fd23dbc16f018
Sergey Tunegov <sergeytunegov@Sergeys-MacBook-Air.local>
2018-09-07 12:10:08 +0300

    Changed color scheme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { PureComponent } from "react";
+import Lottie from "react-lottie";
+import { loader } from "./loader.js";
 import "./App.css";
-import clover from "./images/clover.png";
 import scooter from "./images/scooter.svg";
 
 class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { isStopped: false, isPaused: false };
+  }
+
   render() {
+    const defaultOptions = {
+      loop: false,
+      autoplay: true,
+      animationData: loader,
+      assetsPath: "./images/"
+    };
+
     return (
       <div className="app">
-        <img src={clover} alt="clover" className="clover" />
-        <span className="company-name">Clover Company</span>
-        <span className="cooming-soon">Future is coming...</span>
+        <Lottie
+          options={defaultOptions}
+          height={350}
+          isStopped={this.state.isStopped}
+          isPaused={this.state.isPaused}
+        />
+        {/* <span className="company-name">CLOVER COMPANY</span> */}
+        <span className="cooming-soon">Be part of the future</span>
         <img src={scooter} alt="scooter" className="scooter" />
       </div>
     );
